1e0983478c3413ee12f8ea6590fe857cd1b01093
TomaszOkroy <tomaszokroy@gmail.com>
2024-10-22 13:27:46 +0200

    second

--- miejsca_zerowe/MiejscaZerowe/Program.cs
+++ miejsca_zerowe/MiejscaZerowe/Program.cs
@@ -2,96 +2,144 @@
 
 class Program
 {
-    private enum Options {
-        
-        help,
-        calculate,
-        exit
-    }
-    
+  private enum Options
+  {
 
-  private static void printToUser(string text) {
+    help,
+    calculate,
+    exit
+  }
+
+
+  private static void PrintToUser(string text)
+  {
     Console.WriteLine(text);
   }
 
-  private static string ReadStringFromUser() {
-    // obsługa błędów IOException 
-    return Console.ReadLine();
+  private static string ReadStringFromUser()
+  {
+    string? line = Console.ReadLine();
+    if (line is not null)
+    {
+      return line;
+    }
+    else
+    {
+      return "0";
+    }
   }
 
-  private static void printOptions() {
+  private static double ReadDoubleFromUser()
+  {
+    while (true)
+    {
+      try
+      {
+        double number = double.Parse(ReadStringFromUser());
+        return number;
+
+      }
+      catch (FormatException e)
+      {
+        PrintToUser("Nieprawidłowy format danych wpisz liczbę ponownie");
+        PrintToUser(e.Message);
+      }
+    }
+  }
+
+  private static void PrintOptions()
+  {
     int indexCounter = 0;
-    foreach (Options option in Enum.GetValues(typeof(Options))) {
-        printToUser("" + indexCounter + ". " + option.ToString());
-        indexCounter++;
+    foreach (Options option in Enum.GetValues(typeof(Options)))
+    {
+      PrintToUser("" + indexCounter + ". " + option.ToString());
+      indexCounter++;
     }
-    printToUser("");
-    
+    PrintToUser("");
+
   }
 
-  private static Options ReadOptionFromUser() {
+  private static Options ReadOptionFromUser()
+  {
 
-    while(true) {
-        string userInput = ReadStringFromUser();
-        if(Enum.IsDefined(typeof(Options), userInput)) {
-            return (Options)Enum.Parse(typeof(Options), userInput);   
-        } else {
-            printToUser("Niepoprawna opcja, wpisz ponownie");
-        }
+    while (true)
+    {
+      string userInput = ReadStringFromUser();
+      if (Enum.IsDefined(typeof(Options), userInput))
+      {
+        return (Options)Enum.Parse(typeof(Options), userInput);
+      }
+      else
+      {
+        PrintToUser("Niepoprawna opcja, wpisz ponownie");
+      }
     }
-    
-    
+
+
   }
 
-  private static double CalculateDelta(double a, double b, double c) {
+  private static double CalculateDelta(double a, double b, double c)
+  {
     return Math.Pow(b, 2) - 4 * (a * c);
   }
 
-  private static void CalculateZerosOfFunction() {
+  private static void CalculateZerosOfFunction()
+  {
     bool exitFlag = false;
     double a = 0;
     double b = 0;
     double c = 0;
-    while (exitFlag != true) {
-        printToUser("Wpisz argument a:");
-        a = double.Parse(ReadStringFromUser());
-        if (a == 0) {
-            printToUser("Argumnet a nie może się równać 0");
-        } else {
-            printToUser("Wpisz argument b:");
-            b = double.Parse(ReadStringFromUser());
-            printToUser("Wpisz argument c:");
-            c = double.Parse(ReadStringFromUser());
-            double delta = CalculateDelta(a, b, c);
-            if(delta < 0) {
-                printToUser("Ta funkcja nie ma miejsc zerowych.");
-            } else if(delta > 0) {
-                double firstFunZero = (-b - Math.Sqrt(delta)) / 2 * a;
-                double secondZero = (-b + Math.Sqrt(delta)) / 2 * a;
-                double firstZeroTrancuated = Math.Truncate(firstFunZero * 100) / 100;
-                double secondZeroTrancuated = Math.Truncate(secondZero * 100) / 100;
-                printToUser("Pierwsze miejsce zerowe: " + String.Format("{0:#.##}", firstZeroTrancuated));
-                printToUser("Drugie miejsce zerowe: " + String.Format("{0:#.##}", secondZeroTrancuated));
-            } else {
-                double funZero = -b / 2 * a;
-                double funZeroTrancuated = Math.Truncate(funZero * 100) / 100;
-                printToUser("Funkcja ma tylko jedno miejsce zerowe: " + String.Format("{0:#.##}", funZeroTrancuated));
-            }
-            exitFlag = true;
-
+    while (exitFlag != true)
+    {
+      PrintToUser("Wpisz argument a:");
+      a = ReadDoubleFromUser();
+      if (a == 0)
+      {
+        PrintToUser("Argumnet a nie może się równać 0");
+      }
+      else
+      {
+        PrintToUser("Wpisz argument b:");
+        b = ReadDoubleFromUser();
+        PrintToUser("Wpisz argument c:");
+        c = ReadDoubleFromUser();
+        double delta = CalculateDelta(a, b, c);
+        if (delta < 0)
+        {
+          PrintToUser("Ta funkcja nie ma miejsc zerowych.");
         }
+        else if (delta > 0)
+        {
+          double firstFunZero = (-b - Math.Sqrt(delta)) / 2 * a;
+          double secondZero = (-b + Math.Sqrt(delta)) / 2 * a;
+          double firstZeroTrancuated = Math.Truncate(firstFunZero * 100) / 100;
+          double secondZeroTrancuated = Math.Truncate(secondZero * 100) / 100;
+          PrintToUser("Pierwsze miejsce zerowe: " + String.Format("{0:#.##}", firstZeroTrancuated));
+          PrintToUser("Drugie miejsce zerowe: " + String.Format("{0:#.##}", secondZeroTrancuated));
+        }
+        else
+        {
+          double funZero = -b / 2 * a;
+          double funZeroTrancuated = Math.Truncate(funZero * 100) / 100;
+          PrintToUser("Funkcja ma tylko jedno miejsce zerowe: " + String.Format("{0:#.##}", funZeroTrancuated));
+        }
+        exitFlag = true;
+
+      }
     }
-    
+
 
   }
 
-  private static void ExitProgramm() {
-    printToUser("koniec programu bye bye!");
-    
+  private static void ExitProgramm()
+  {
+    PrintToUser("koniec programu bye bye!");
+
   }
 
-  private static void PrintHelpToUser() {
-    printToUser("""
+  private static void PrintHelpToUser()
+  {
+    PrintToUser("""
         Celem tego programu jest obliczanie miejsc zerowych funkcji kwadratowej.
         Aby rozpocząć wpisz opcję calculate.
         Następnie wpisz argumenty a, b i c.
@@ -99,37 +147,40 @@ class Program
         """);
   }
 
-  private static void MainLoop() {
-    
+  private static void MainLoop()
+  {
+
     bool exitLoop = false;
 
-    while(exitLoop != true) {
-        printToUser("Dostępne opcje:");
-        printOptions();
-        printToUser("Wprowadz opcję...");
-        Options userOption = ReadOptionFromUser();
-        switch(userOption) {
-            case Options.help:
-                PrintHelpToUser();
-                break;
-            case Options.calculate:
-                CalculateZerosOfFunction();
-                break;
-            case Options.exit:
-                exitLoop = true;
-                ExitProgramm();
-                break;
-                
-        }
-            
-        
+    while (exitLoop != true)
+    {
+      PrintToUser("Dostępne opcje:");
+      PrintOptions();
+      PrintToUser("Wprowadz opcję...");
+      Options userOption = ReadOptionFromUser();
+      switch (userOption)
+      {
+        case Options.help:
+          PrintHelpToUser();
+          break;
+        case Options.calculate:
+          CalculateZerosOfFunction();
+          break;
+        case Options.exit:
+          exitLoop = true;
+          ExitProgramm();
+          break;
+
+      }
+
+
 
     }
   }
 
   static void Main(string[] args)
   {
-    
+
     Console.WriteLine("Witaj w programie do oblicznia miejsc zerowaych funkcji kwadratowej!\n");
     MainLoop();
   }
---
+++ zad_2/Zad2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq.Expressions;
+
+class Program
+{
+
+    private static void PrintToUser(string text)
+    {
+        Console.WriteLine(text);
+    }
+
+    private static string ReadStringFromUser()
+    {
+        while (true)
+        {
+
+            string? result = Console.ReadLine();
+            if (result is not null)
+            {
+                return result;
+            }
+
+
+        }
+
+    }
+
+    private static int ReadIntFromUser()
+    {
+        while (true)
+        {
+            try
+            {
+                int number = int.Parse(ReadStringFromUser());
+                return number;
+
+            }
+            catch (FormatException e)
+            {
+                PrintToUser("Nieprawidłowy format danych wpisz liczbę ponownie");
+                PrintToUser(e.Message);
+            }
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+
+        PrintToUser("Podaj liczbę rozpoczynającą ciąg:");
+        int start = ReadIntFromUser();
+        PrintToUser("Podaj liczbę kończącą ciąg:");
+        int end = ReadIntFromUser();
+        PrintToUser("Ciąg liczb od:" + start + " do:" + end);
+        for (int i = start; i <= end; i++)
+        {
+            PrintToUser(i.ToString());
+        }
+
+    }
+}
+
